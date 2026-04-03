e96eeaba6fe2f69739e70b084ae2f12cac7f7d58
Craftgirlsss <craftgirlsssshopping@gmail.com>
2026-04-04 00:31:49 +0700

    audit security

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,6 +30,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'password_changed_at' => now(),
+            'status' => 'active',
             'remember_token' => Str::random(10),
         ];
     }
